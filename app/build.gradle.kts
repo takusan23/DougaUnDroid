6eaf54d83064be9bf95741754a348900956ee843
takusan23 <takumu1200@gmail.com>
2024-12-06 00:10:29 +0900

    2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.takusan23.dougaundroid"
         minSdk = 21
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
