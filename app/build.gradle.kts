75bb32964b5bf821715de75f8c5e1c6ac573efb9
takusan23 <takumu1200@gmail.com>
2026-01-15 01:25:22 +0900

    viewmodel つかってなかった

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,6 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation3.ui)
     implementation(libs.androidx.navigation3.runtime)
-    implementation(libs.androidx.lifecycle.viewmodel.navigation3)
     implementation(libs.kotlinx.serialization.json)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
